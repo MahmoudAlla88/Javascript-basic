2122fa9a180a9b800930365479a12ad527926bc6
MahmoudAlla88 <hamoodaalla88@gmail.com>
2024-12-26 16:11:27 +0300

    math

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// q1
+console.log(-5*3);
+console.log("JavaScript"+50);
+console.log(17%5);
+console.log(5%17);
+console.log(5/10);
+console.log(4==='4');
+console.log(4!=5);
+console.log(7<=8);
+console.log("Hello"+5);
+let x = 4.7;
+console.log(Math.ceil(x) - Math.floor(x));
+x = 3;
+console.log(Math.ceil(x) - Math.floor(x)); 
+Math.pow(x,2)
+
+
+//q2
+
+let number = prompt("Enter a number:");
+
+console.log("You entered: " + number);
+
+//q3
+let number1 = prompt("Please enter num1:",);
+let number2 = prompt("Please enter num2:",);
+let sum=+number1+ +number2;
+console.log("The result of sim " + sum);
+
+//q4
+
+let mul=1;
+let num1= prompt("Please enter your number:",);
+let num2 = prompt("Please enter your number:",);
+mul=num1*num2;
+console.log("The result of mul  " + mul);
+
+//q5
+let div=1;
+let n1= prompt("Please enter your number:",);
+let n2 = prompt("Please enter your number:",);
+div=+num1/+num2;
+console.log("The result of div " + div);
+
+
+// q6
+let sum1 = 5 + 6;
+let sum2= prompt("num1+num2:",sum1);
